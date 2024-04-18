2a7579f7dca261707ecd0e3430a30e2511a02459
Thomas Lohse <thomas.k.lohse@gmail.com>
2024-04-18 09:34:31 +0200

    ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("com.diffplug.spotless") version "6.19.0"
 }
 
 android {
@@ -42,3 +43,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+spotless {
+    kotlin {
+        ktfmt("0.44").dropboxStyle()
+    }
+}
+
